803423a88aeddb75f7365d99aabb498ad43e18af
Brass Clarkson <bm.clarkson@gmail.com>
2018-05-03 00:51:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,18 @@
+# user_input = gets.chomp
+# number_entered = user_input.to_i - 1
+
+# def valid_move?(number_entered, board)
+#   number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+# end
+
 user_input = gets.chomp
 number_entered = user_input.to_i - 1
 
-def valid_move?(number_entered, board)
+def valid_move?(number_entered)
   number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
+
+
 def position_taken?(board, index)
   
   !  (board[index] ==  " " || board[index] == "" || board[index] == nil) 
